d16019c6e1c5cc4a1569149a79baa71d2792564c
ginosian <marta.ginosian@gmail.com>
2017-01-11 18:15:42 +0400

    component added successfuly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `
+    <h1>Inline other template</h1>
+    <fa-other></fa-other>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
